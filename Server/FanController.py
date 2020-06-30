42701107e63abbf4b5b93ee82a6adf79eb9d4384
justinmichlewis <justinmichlewis@gmail.com>
2020-06-30 11:17:30 -0700

    Cleaned up files and added overviews

--- Server/FanController.py
+++ Server/FanController.py
@@ -1,60 +1,97 @@
+###################################################################
+# Fan Controller runs on an infiniate loop to start and stop	  #
+# the exhaust fans in the fruiting chamber. Fan controller checks #
+# settings.json file to retreive when to run and for how log. 	  #
+# settings.json file is defined via the web app and writted to    #
+# via restcontroller.js.										  #
+#																  #
+# June 30, 2020													  #
+###################################################################
+
 import RPi.GPIO as gpio
 import json
 from datetime import datetime
 import time
 import os
+import json
 
-#GPIO SETTINGS
+# GPIO SETTINGS
 gpio.setmode(gpio.BCM)
 FAN1_PIN = 3
 FAN2_PIN = 21
-gpio.setup(FAN1_PIN,gpio.OUT)
-gpio.setup(FAN2_PIN,gpio.OUT)
+gpio.setup(FAN1_PIN, gpio.OUT)
+gpio.setup(FAN2_PIN, gpio.OUT)
 gpio.setwarnings(False)
 
 os.system('clear')
 
 while 1:
-	now = datetime.now()
-	print('----------------------')
-	print('Running fan controller')
-	print(now)
-	print('----------------------')
-	#TIME
+    now = datetime.now()
+    print('----------------------')
+    print('Running fan controller')
+    print(now)
+    print('----------------------')
 
-	print('Current Hour',now.hour)
+    # LOAD JSON SETTINGS FILE
+    with open('settings.json') as f:
+        data = json.load(f)
 
-	#LOAD JSON SETTINGS FILE
-	with open('settings.json') as f:
-		data = json.load(f)
+    # OPEN LOGFILE
+    logFile = open('Fanlog.log', 'a')
+    logLine = ''
+    print('Times:', data['times'], 'Duration:', data['duration'])
 
-	print('Times:', data['times'], 'Duration:', data['duration'])
+    # RUN FAN IF CURRENT HOUR MATCHES SETTINGS AN
+    hour = 3600  # HOUR IN SECONDS
+    currentHour = now.hour
+    durationSec = 60 * data['duration']
+    for t in data['times']:
+        if t == currentHour:
+            startTime = datetime.now()
 
-	#RUN FAN IF CURRENT HOUR MATCHES SETTINGS AN
-	hour = 3600 #HOUR IN SECONDS
-	currentHour = 12#now.hour
-	durationSec = 15#60 * data['duration']
-	for t in data['times']:
-		if t == currentHour:
-			print('Starting Fan for ', data['duration'], ' min', 'at ', now)
-			gpio.output(FAN1_PIN, True)
-			gpio.output(FAN2_PIN, True)
-			#SLEEP FOR DURATOIN OF FAN TO RUN THEN STOP
-			##print("1", gpio.input(FAN1_PIN))
-			#print("2", gpio.input(FAN2_PIN))
-			time.sleep(durationSec)
-			gpio.output(FAN1_PIN, False)
-			gpio.output(FAN2_PIN, False)
-			#print("1", gpio.input(FAN1_PIN))
-			#print("2", gpio.input(FAN2_PIN))
-			print('Fan has Stopped at ', now)
-			#SLEEP FOR 1 HOUR AND CHECK AND RUN AGAIN IF APPLICABLE
+            print('Starting Fan for ',
+                  data['duration'], ' min', 'at ', startTime)
 
-		else: 
-			x = 1
-			#print('Not Time')
-	print('Sleeping for 1 hour at', now)
-	#ACCOUNT FOR THE TIME ELAPSED DURING FAN ON
-	time.sleep(hour - durationSec)
-			
-#gpio.output(FAN_PIN, True)
+            gpio.output(FAN1_PIN, True)
+            gpio.output(FAN2_PIN, True)
+
+            time.sleep(durationSec)
+
+            gpio.output(FAN1_PIN, False)
+            gpio.output(FAN2_PIN, False)
+
+            print('Fan has Stopped at ', datetime.now())
+            timeDiff = str(datetime.now() - startTime)
+
+            logLine = startTime.strftime("%m/%d/%Y %H:%M:%S") + ' | ' + datetime.now().strftime(
+                "%m/%d/%Y  %H:%M:%S") + ' | ' + timeDiff + ' | ' + str(data['duration']) + " | " + str(data['times']) + "; \n"
+
+            logFile.write(logLine)
+        else:
+            p = 1  # BS CODE FOR NOW
+
+    print('Sleeping for 1 hour at', now)
+    # ACCOUNT FOR THE TIME ELAPSED DURING FAN ON
+    sleepTime = datetime.now()
+    time.sleep(hour - durationSec)
+
+
+################################################
+# BELOW THIS IS ANTIQUATED CODE SAVE IF NEEDED #
+################################################
+    # logLine = {
+    #	"start": startTime.strftime("%m/%d/%Y %H:%M:%S"),
+    #	"end": datetime.now().strftime("%m/%d/%Y  %H:%M:%S"),
+    #	"actualDurr": str(timeDiff)[0:7],
+    #	"scheduledDurr": str(data['duration']),
+    #	"runTimes": str(data['times'])
+    # }
+    # print(json.dumps(logLine))
+    # logFile.write(json.dumps(logLine))
+
+    # print(writeLog)
+    # if writeLog:
+    #	sleepDur =  str(datetime.now() - sleepTime) + "\n"
+    #	output = logLine + ' | '+ sleepDur + ';'
+    #	logFile.write(output)
+    #	print("Log W to File")
